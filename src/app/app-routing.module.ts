870c5e03ce7b2145293975d5d0f75ca8216b6f5b
Tobias Sindek <tobias.sindek@gmail.com>
2022-09-28 15:35:43 +0200

    feat: implement routing between book-page and admin-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListPageComponent } from './pages/book-list-page/book-list-page.component';
+import { AdministrationPageComponent } from './pages/administration-page/administration-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BookListPageComponent },
+  { path: 'administration', component: AdministrationPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
